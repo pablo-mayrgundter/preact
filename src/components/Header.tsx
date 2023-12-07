1953db75af66bcb5fdf8bf090d644ee3161c79cf
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2023-12-07 23:45:45 +0100

    Re-init with "bun create preact"

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import { useLocation } from 'preact-iso'
+
+
+/**
+ * @return {object} The page header
+ */
+export function Header() {
+  const { url } = useLocation()
+
+  return (
+    <header>
+      <nav>
+        <a href="/" class={url === '/' && 'active'}>
+          Home
+        </a>
+        <a href="/404" class={url === '/404' && 'active'}>
+          404
+        </a>
+      </nav>
+    </header>
+  )
+}
