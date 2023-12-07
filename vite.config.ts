a101feb02120b1ba18163c4d8c6ea302d6bd3dff
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2023-12-08 00:23:20 +0100

    Change repo/project name to preact-router-vite-bun.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  base: '/preact/',
+  base: `/preact-router-vite-bun/`,
   build: {
     outDir: './docs',
   },
